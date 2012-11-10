68cdb335700905c244517fd11909deabcf77093f
Pau Rosello <paurosello@gmail.com>
2012-11-10 18:46:01 +0100

    Template Inicio

--- encuestas/views.py
+++ encuestas/views.py
@@ -1,12 +1,14 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from encuestas.models import Votacion
 
 def inicio(request):
     csrfContext = RequestContext(request)
     return render_to_response(
         'portada.html',
         {
+            'votaciones' : Votacion.objects.all()
         },
         csrfContext
     )
