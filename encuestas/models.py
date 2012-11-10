9d4a3fa6c377181de075d09160266f6a4cc64656
Pau Rosello <paurosello@gmail.com>
2012-11-10 18:22:46 +0100

    Creamos los modelos

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Votacion(models.Model):
+    titulo = models.CharField(max_length=150, primary_key=True)
+    activa = models.BooleanField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.titulo
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.titulo)
+        super(Votacion, self).save(*args, **kwargs)
+
+admin.site.register(Votacion)
+
+class Voto(models.Model):
+    votacion = models.ForeignKey(Votacion)
+    opcion = models.BooleanField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.votacion + " " + self.opcion
+
+admin.site.register(Voto)
