b2b642a09f06ae0722494d2ab051c975d7c6aeec
Pau Rosello <paurosello@gmail.com>
2012-11-11 09:08:17 +0100

    Añadimos vistas y URL

--- acmtut/urls.py
+++ acmtut/urls.py
@@ -7,6 +7,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'encuestas.views.inicio', name='inicio'),
+    url(r'^activas$', 'encuestas.views.activas', name='inicio'),
+    url(r'^votar/(\d)/(\d)$', 'encuestas.views.realizar_voto'),
+    url(r'^votacion/(\d)$', 'encuestas.views.votacion'),
+
     # url(r'^acmtut/', include('acmtut.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- encuestas/views.py
+++ encuestas/views.py
@@ -1,14 +1,50 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from encuestas.models import Votacion
+from encuestas.models import Votacion, Voto
 
 def inicio(request):
-    csrfContext = RequestContext(request)
     return render_to_response(
         'portada.html',
         {
             'votaciones' : Votacion.objects.all()
         },
-        csrfContext
+    )
+
+def activas(request):
+    votaciones = Votacion.objects.filter(activa=True)
+
+    return render_to_response(
+
+        'portada.html',
+        {
+            'nvotaciones' : votaciones.count(),
+            'votaciones' : votaciones
+        },
+    )
+
+def realizar_voto(request, votacion, voto):
+
+    voto_realizado = Voto(votacion=Votacion.objects.get(pk=votacion), opcion=int(voto)==1)
+    voto_realizado.save()
+
+    return render_to_response(
+        'voto_realizado.html',
+        {
+            'voto':voto_realizado
+        },
+    )
+
+def votacion(request, vot_id):
+    rvotacion = Votacion.objects.get(pk=vot_id)
+
+    votos = Voto.objects.filter(votacion=rvotacion)
+
+    return render_to_response(
+        'votacion.html',
+        {
+            'votacion' : rvotacion,
+            'votosFavor' : votos.filter(opcion=True),
+            'votosContra' : votos.exclude(opcion=True)
+        },
     )
